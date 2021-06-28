720b25ae2f3bd30a8fa909350c143de83d17054a
netpanik <netpanik@gmail.com>
2021-06-28 21:52:57 +0300

    Fixed git

---
+++ experiments/ExamplesCheck/ExamplesCheck/PermutationString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ExamplesCheck
+{
+    public class PermutationString
+    {
+        string _string = string.Empty;
+        string _prefix = string.Empty;
+
+        public PermutationString(string str)
+        {
+            _string = str;
+        }
+
+        public void run()
+        {
+            run(_string, _prefix);
+        }
+
+        void run(string str, string prefix)
+        {
+            if(str.Length == 0)
+            {
+                System.Console.WriteLine(prefix);
+            }
+            else
+            {
+                for(var i = 0; i < str.Length; ++i)
+                {
+                    var rem = str.Substring(0, i) + str.Substring(i + 1);
+                    run(rem, prefix + str[i]);
+                }
+            }
+        }
+    }
+}
---
+++ experiments/ExamplesCheck/ExamplesCheck/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExamplesCheck
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var permutationString = new PermutationString("abc");
+            permutationString.run();
+        }
+    }
+}
